c0ad9766f93c3d5f2d9642ce0f488c2a93706d5a
Ivan <ivan.bondarenko.it@gmail.com>
2023-03-10 14:47:14 +0400

    blog app: urls added

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('/', views.starting_page, name="starting-page"),
+    path('posts', views.pasts, name="post-page"),
+    path('posts/<slug:slug>', views.post_detail,
+         name="post-detail-page")
+]
