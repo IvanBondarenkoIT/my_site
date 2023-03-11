0622f2b40335b13904189a25492d51a2a3da6f3c
Ivan <ivan.bondarenko.it@gmail.com>
2023-03-11 10:22:30 +0400

    blog app: app index static css and html

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('/', views.starting_page, name="starting-page"),
+    path('', views.starting_page, name="starting-page"),
     path('posts', views.pasts, name="post-page"),
     path('posts/<slug:slug>', views.post_detail,
          name="post-detail-page")
