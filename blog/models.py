ba3517ce191f108061f9b55cfc9f377b6a832fc9
Ivan <ivan.bondarenko.it@gmail.com>
2023-03-19 23:21:51 +0400

    blog app: 19.03 models admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.core.validators import validate_email, MinValueValidator
+
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    # image = models.ImageField()
+    image_name = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinValueValidator])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-date']
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+
